0e0c7ec2cc54222a8161619ab0c66e68ebba5f03
jordan <jtran0817@gmail.com>
2025-05-09 13:40:29 -0700

    Added the initialization of modItems

---
+++ src/main/java/com/limitd/harvest_helpers/Item/ModItems.java
@@ -0,0 +1,21 @@
+package com.limitd.harvest_helpers.Item;
+
+import com.limitd.harvest_helpers.HarvestHelpers;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+public class ModItems
+{
+        public static final Item ORANGE_SAPLING = registerItem("orange_sapling", new Item(new Item.Settings()));
+
+        private static Item registerItem(String name, Item item)
+        {
+                return Registry.register(Registries.ITEM, Identifier.of(HarvestHelpers.MOD_ID, name), item);
+        }
+        public static void registerModItems()
+        {
+                HarvestHelpers.LOGGER.info("registering Mod items for " + HarvestHelpers.MOD_ID);
+
+        }
+}
